964b1c42363d2807b5bee908dd88d2db9b9c2c66
Ahmad Awais <me@AhmadAwais.com>
2020-04-03 09:08:33 +0500

    👌 IMPROVE: Charts

--- utils/getCountryChart.js
+++ utils/getCountryChart.js
@@ -1,10 +1,13 @@
+const comma = require('comma-number');
 const handleError = require('cli-handle-error');
-var axios = require('axios');
+const axios = require('axios');
 const to = require('await-to-js').default;
 const moment = require('moment');
+const blessed = require('blessed');
+const contrib = require('blessed-contrib');
 
-module.exports = async (spinner, countryName, options) => {
-	if (countryName && options.chart) {
+module.exports = async (spinner, countryName, { chart, log }) => {
+	if (countryName && chart) {
 		const [err, response] = await to(
 			axios.get(`https://corona.lmao.ninja/v2/historical/${countryName}`)
 		);
@@ -18,50 +21,49 @@ module.exports = async (spinner, countryName, options) => {
 			);
 			process.exit(0);
 		}
-		var logScale = x => x;
-		if (options.log) {
+		const logScale = x => x;
+		if (log) {
 			logScale = x => (x === 0 ? undefined : Math.log(x));
 		}
-		var shortDate = x => moment(x, 'MM/DD/YY').format('D MMM');
-		var cumulative = (a, b) => (a = a + b);
-		var blessed = require('blessed'),
-			contrib = require('blessed-contrib'),
-			screen = blessed.screen(),
-			line = contrib.line({
-				style: {
-					text: 'white',
-					baseline: 'white'
-				},
-				xLabelPadding: 10,
-				abbreviate: true,
-				xPadding: 1,
-				showLegend: true,
-				wholeNumbersOnly: false, //true=do not show fraction in y axis
-				label: countryName
-			});
-		var dates = Object.keys(response.data.timeline.cases).map(shortDate);
-		var cases = Object.values(response.data.timeline.cases);
-		var deaths = Object.values(response.data.timeline.deaths);
-		var recovered = Object.values(response.data.timeline.recovered);
+		const shortDate = x => moment(x, 'MM/DD/YY').format('D MMM');
+		const cumulative = (a, b) => (a = a + b);
+		const screen = blessed.screen();
+		const line = contrib.line({
+			style: {
+				text: 'white',
+				baseline: 'white'
+			},
+			xLabelPadding: 10,
+			xPadding: 3,
+			abbreviate: true,
+			showLegend: true,
+			legend: { width: 20 },
+			wholeNumbersOnly: false,
+			label: countryName.toUpperCase()
+		});
+		const dates = Object.keys(response.data.timeline.cases).map(shortDate);
+		const cases = Object.values(response.data.timeline.cases);
+		const deaths = Object.values(response.data.timeline.deaths);
+		const recovered = Object.values(response.data.timeline.recovered);
 
-		var casesSeries = {
-			title: `cases__${cases[cases.length - 1]}`,
+		const casesSeries = {
+			title: `CASES: ${comma(cases[cases.length - 1])}`,
 			x: dates,
 			y: cases.map(logScale),
 			style: {
 				line: 'cyan'
 			}
 		};
-		var deathsSeries = {
-			title: `deaths__${deaths[deaths.length - 1]}`,
+		const deathsSeries = {
+			title: `DEATHS: ${comma(deaths[deaths.length - 1])}`,
 			x: dates,
 			y: deaths.map(logScale),
 			style: {
 				line: 'red'
 			}
 		};
-		var recoveredSeries = {
-			title: `recovered__${recovered[deaths.length - 1]}`,
+		const recoveredSeries = {
+			title: `RECOVERED: ${comma(recovered[deaths.length - 1])}`,
 			x: dates,
 			y: recovered.map(logScale),
 			style: {
@@ -71,10 +73,10 @@ module.exports = async (spinner, countryName, options) => {
 
 		screen.append(line);
 		spinner.stop();
-		line.setData([casesSeries, recoveredSeries, deathsSeries]);
+		line.setData([casesSeries, deathsSeries, recoveredSeries]);
 		screen.render();
 		await new Promise((resolve, _) => {
-			screen.key(['escape', 'q', 'C-c', 'enter'], function (ch, key) {
+			screen.key(['escape', 'q', 'C-c', 'enter', 'space'], (ch, key) => {
 				return process.exit(0);
 			});
 		});
