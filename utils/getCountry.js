9c21f982870a925b2e95e4dd7696453ead510292
JEONYUNTAE <67727745+JEONYUNTAE@users.noreply.github.com>
2020-11-24 19:00:50 +0900

    Update getCountry.js

--- utils/getCountry.js
+++ utils/getCountry.js
@@ -3,6 +3,7 @@ const numberFormat = require('./numberFormat');
 const exitCountry = require('./exitCountry');
 const to = require('await-to-js').default;
 const handleError = require('cli-handle-error');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 module.exports = async (spinner, table, states, countryName, options) => {
 	if (countryName && !states && !options.chart) {
@@ -30,6 +31,47 @@ module.exports = async (spinner, table, states, countryName, options) => {
 			format(thisCountry.casesPerOneMillion)
 		]);
 		spinner.stopAndPersist();
+		
+		// 파일명 country_chart를 thisCountry.country로 받아온 변수로 수정할 계획
+		
+		var fs=require('fs');
+
+		if (!fs.existsSync('./output')){
+			fs.mkdirSync('./output');
+		}
+
+		console.log(thisCountry);
+		
+		var path_ =  'output/'+thisCountry.country+'.csv';
+		
+		const csvWriter = createCsvWriter({
+			path: path_,
+			header: [
+			  {id: 'cases', title: 'Cases'},
+			  {id: 'todayCases', title: 'TodayCases'},
+			  {id: 'deaths', title: 'Deaths'},
+			  {id: 'todayDeaths', title: 'Deaths (today)'},
+			  {id: 'recovered', title: 'Recovered'},
+			  {id: 'active', title: 'Active'},
+			  {id: 'critical', title: 'Critical'},
+			  {id: 'casesPerOneMillion', title: 'CasesPerOneMillion'},
+			]
+		});
+		
+		const data=[
+			{
+				cases: thisCountry.cases,
+				todayCases: thisCountry.todayCases,
+				deaths: thisCountry.deaths,
+				todayDeaths: thisCountry.todayDeaths,
+				recovered: thisCountry.recovered,
+				active: thisCountry.active,
+				critical: thisCountry.critical,
+				casesPerOneMillion: thisCountry.casesPerOneMillion
+			}
+		]
+
+		csvWriter.writeRecords(data);
 		console.log(table.toString());
 	}
 };
