98651f550af5d444fa819f516127dbf34177007c
brendancarey19 <64050030+brendancarey19@users.noreply.github.com>
2021-04-25 19:07:17 -0400

    Add files via upload
    added functionality to access individual states from command line instead of all 50

--- utils/getCountry.js
+++ utils/getCountry.js
@@ -6,30 +6,63 @@ const handleError = require('cli-handle-error');
 
 module.exports = async (spinner, table, states, countryName, options) => {
 	if (countryName && !states && !options.chart) {
-		const [err, response] = await to(
-			axios.get(`https://corona.lmao.ninja/v2/countries/${countryName}`)
+
+		const [err, response]  = await to(
+			axios.get(`https://corona.lmao.ninja/v2/states/${countryName}`)
 		);
-		exitCountry(err, spinner, countryName);
-		err && spinner.stopAndPersist();
-		handleError(`API is down, try again later.`, err, false);
-		const thisCountry = response.data;
 
-		// Format.
-		const format = numberFormat(options.json);
+		if (
+			err &&
+			err.response &&
+			err.response.status &&
+			err.response.status === 404
+		) {
+			console.log("hello")
+			const [err, response] = await to(
+				axios.get(`https://corona.lmao.ninja/v2/countries/${countryName}`)
+			);
+			exitCountry(err, spinner, countryName);
+			err && spinner.stopAndPersist();
+			handleError(`API is down, try again later.`, err, false);
+			const thisCountry = response.data
+			// Format.
+			const format = numberFormat(options.json);
 
-		table.push([
-			`—`,
-			thisCountry.country,
-			format(thisCountry.cases),
-			format(thisCountry.todayCases),
-			format(thisCountry.deaths),
-			format(thisCountry.todayDeaths),
-			format(thisCountry.recovered),
-			format(thisCountry.active),
-			format(thisCountry.critical),
-			format(thisCountry.casesPerOneMillion)
-		]);
-		spinner.stopAndPersist();
-		console.log(table.toString());
+			table.push([
+				`—`,
+				thisCountry.country,
+				format(thisCountry.cases),
+				format(thisCountry.todayCases),
+				format(thisCountry.deaths),
+				format(thisCountry.todayDeaths),
+				format(thisCountry.recovered),
+				format(thisCountry.active),
+				format(thisCountry.critical),
+				format(thisCountry.casesPerOneMillion)
+			]);
+			spinner.stopAndPersist();
+			console.log(table.toString());
+		}
+
+		// input is a state
+		else {
+			const thisState = response.data;
+			const format = numberFormat(options.json);
+			table.push([
+				`—`,
+				thisState.state,
+				format(thisState.cases),
+				format(thisState.todayCases),
+				format(thisState.deaths),
+				format(thisState.todayDeaths),
+				format(thisState.recovered),
+				format(thisState.active),
+				"N/A",
+				format(thisState.casesPerOneMillion)
+			]);
+			spinner.stopAndPersist();
+			console.log(table.toString());
+
+		}
 	}
 };
